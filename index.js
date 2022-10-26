9242f8a048e08a39378e0870005ba2eb2d79a5c0
Mathew Emmanuel <mathewemmanuel212@gmail.com>
2022-10-26 10:33:17 -0700

    I made a change  to this repo requiring middleware

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const app = express();
 
 app.use(json());
 
-app.use("/", routes);
+mongoose.Promise = global.Promise;
+//creating static middleware..
+
+
+
+app.use(bodyParser.json());
+//setting up our routes for express to know we ant to use the route..
+//this initialize the routes..
+app.use("/api", routes);
+//creating middle ware for error handling
+app.use(function (err, req, res, next) {
+  //console.log(err)
+  res.status(400).send({ error: err.message });
+});
+// app.get('/',function(req,res){
+//   console.log('GEt request')
+//   res.send({name:'mathew'});
+// })
+//listening for request..
+//when deploying this app there might be a environment variable which provides ..
+//port to listen to so we will provide one
+
+
 
 const port = process.env.PORT || 3000;
 
