06968487f9313e0b24399eb42d798d652f85b517
Mathew Emmanuel <mathewemmanuel212@gmail.com>
2022-10-26 11:01:30 -0700

    changes made for the booking flight api are creating a middleware to the routes and also an api from a database

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express");
-const { json } = require("express");
-const flights = require("./controllers/flightController");
-const models = require("./models/Flight");
-const routes = require("./routes/flightRoute");
-
+// const routes = require('./routes/api')
+//requireing parser..
+const bodyParser = require("body-parser");
+//connecting to mngoose
+const mongoose = require("mongoose");
+//setting up express app
 const app = express();
-
-app.use(json());
-
+const dBURI =
+  "mongodb+srv://++++:*****@cluster1.avjfq.mongodb.net/******?retryWrites=true&w=majority";
+mongoose
+  .connect(dBURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+//converting mongoose promise to global promise.
 mongoose.Promise = global.Promise;
 //creating static middleware..
-
-
-
-app.use(bodyParser.json());
+app.use(express.static("public"));
 //setting up our routes for express to know we ant to use the route..
+app.use(bodyParser.json());
 //this initialize the routes..
-app.use("/api", routes);
+app.use("/api", require("./routes/api"));
 //creating middle ware for error handling
 app.use(function (err, req, res, next) {
   //console.log(err)
@@ -29,11 +32,6 @@ app.use(function (err, req, res, next) {
 //listening for request..
 //when deploying this app there might be a environment variable which provides ..
 //port to listen to so we will provide one
-
-
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.port || 4000, function () {
+  console.log("Hey We are listening for request");
 });
--- models/Flight.js
+++ models/Flight.js
@@ -1,1 +1,27 @@
-exports.exampleModel = [];
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// "geometry": {
+//    "type": "Point",
+//    "coordinates": [125.6, 10.1]
+//  }
+
+// creating our flight schema
+const flightSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    time: { type: String, required: true },
+    price: {
+      type: String,
+      required: true,
+    },
+    Date: [Date],
+  },
+
+  { timestamps: true }
+);
+//sharing a blog collection from my database
+const Blog = mongoose.model("Blog", flightSchema);
+module.exports = Blog;
+
+//creating our ninja / users model to represent our collections..
